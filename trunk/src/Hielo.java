6abb0ba85c31ac969dc2fb46883c7c9f3770d678
gygounet@gmail.com <gygounet@gmail.com@b1101464-85f2-11dd-b6e4-1fb4a74862d0>
2008-10-13 23:15:31 +0000

    Agrego Primera Version de Hielo.java, creo q esta bien hecha, si pueden darle una mirada mejor.

--- trunk/src/Hielo.java
+++ trunk/src/Hielo.java
@@ -13,8 +13,11 @@ public class Hielo implements Terreno{
 	 * @param pooglin
 	 */
 	public void accionarTerreno(Personaje pooglin){
-		
-		
+		Velocidad velocidad=((Pooglin)pooglin).getVelocidad();
+		int moduloVelocidad=velocidad.getModulo();
+		int duplicaVelocidad=moduloVelocidad * 2;
+		velocidad.setModulo(duplicaVelocidad);
+		((Pooglin)pooglin).setVelocidad(velocidad);
 	}
 	/** Este metodo no lo implemento por no ser necesario para
 	 * el desarrollo de esta clase, solamente lo redefino porque esta
