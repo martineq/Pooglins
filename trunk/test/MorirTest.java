29a731c4492f579e9dcb16ee05e393d86433373c
gygounet@gmail.com <gygounet@gmail.com@b1101464-85f2-11dd-b6e4-1fb4a74862d0>
2008-10-19 21:41:25 +0000

    Agrego MorirTest...la prueba en Junit de la clase Morir.java, prueba todo lo que la clase hace hasta el momento, si es necesario probar algo mas avisenme.

---
+++ trunk/test/MorirTest.java
@@ -0,0 +1,35 @@
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author guido
+ *
+ */
+public class MorirTest extends TestCase {
+
+	private Pooglin unPooglin;
+	private Morir morir;
+	
+	protected void setUp(){
+		unPooglin = new Pooglin(0,0);
+		morir = new Morir();
+	}
+
+	public void testUtilizarMorir() {
+		Velocidad velocidad = unPooglin.getVelocidad();
+		velocidad.setVelocidadX(6);
+		velocidad.setVelocidadY(0);
+		unPooglin.setVelocidad(velocidad);
+		morir.utilizar(unPooglin);
+		boolean vive=unPooglin.estaVivo();
+		assertFalse(vive);
+		velocidad=unPooglin.getVelocidad();
+		assertEquals(0,velocidad.getVelocidadX());
+		assertEquals(0,velocidad.getVelocidadY());
+	}
+
+}
