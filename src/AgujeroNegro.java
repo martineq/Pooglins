c5d456243c1fd690f32cc9503e801cc01580dc34
gygounet <gygounet@gmail.com>
2008-11-16 18:11:44 +0000

    Primera version de AgujeroNegro.java, la estoy desarrollando, es simplemente para que vean la idea. Agrego metodo a Nivel.java; esta en desarrollo lo hice feo para ver que sirva.Para seguir necesito aplicar Singleton en Nivel.

--- src/AgujeroNegro.java
+++ src/AgujeroNegro.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+
 /**Clase que representa a los bloques de terreno donde hay un agujero negro.-
- * @author lkolaric
+ * @author guido
  * @since 15/10/08
  * 
  */
 public class AgujeroNegro extends Terreno {
-
+	//REFERENCIA PARA USAR NIVEL COMO SINGLETON.-
+	//FALTA APLICAR ESE PATRON.-
+	//EN DESARROLLO.-
+	//Guido.-
+	private Nivel nivel;
+	
 	/**Constructor de AgujeroNegro.-
 	 * 
 	 */
@@ -15,7 +25,21 @@ public class AgujeroNegro extends Terreno {
 	}
 
 	public void accionarTerreno(Personaje pooglin) {
-		
+		//Comento...Cuando Nivel sea Singleton obtengo una instancia.-
+		//Guido.-...Desarrollandolo...esperando el ejemplo de Pablo 
+		//Suarez.-
+		this.nivel = new Nivel();//Instancio una referencia al nivel ya que es unica
+		 //por ser un Singleton.
+		ArrayList personajesCercanos = nivel.obtenerPooglinsCercanos(this.getPosicionX(),this.getPosicionY());
+		Iterator it = personajesCercanos.iterator();
+		while (it.hasNext()){
+			Pooglin pooglinCercano = (Pooglin) it.next();
+			pooglinCercano.setPosicionX(this.getPosicionX());//coloco al pooglin en el agujero negro
+			pooglinCercano.setPosicionY(this.getPosicionY());
+			Habilidad morir = pooglinCercano.getMatarse();
+			morir.utilizar(pooglin);//mato o succiono al pooglin.
+
+}
 	}
 	
     /* ¿Se va a usar?
