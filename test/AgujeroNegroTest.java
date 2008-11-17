ecf1e8ed7a1abcc55fbd4b30d880a0dbd6cb5b2d
gygounet <gygounet@gmail.com>
2008-11-17 12:49:42 +0000

    Agrego AgujeroNegroTest...todavia no esta desarrollada pero subo el esqueleto para no olvidarme de hacerla.

---
+++ test/AgujeroNegroTest.java
@@ -0,0 +1,24 @@
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author guido
+ *
+ */
+public class AgujeroNegroTest {
+
+	private Nivel nivel=null;
+
+	public void setUp(){
+		nivel=Nivel.getInstance();
+		
+		
+	}
+	public void testAccionarTerreno(){
+		
+	}
+}
