163c81607e23cf968125398b18446b97c2ade53e
gygounet@gmail.com <gygounet@gmail.com@b1101464-85f2-11dd-b6e4-1fb4a74862d0>
2008-11-19 01:26:08 +0000

    Modifico detalles de prolijidad, no puedo solucionar el problema en AgujeroNegroTest.java...sigo con eso

--- trunk/test/AgujeroNegroTest.java
+++ trunk/test/AgujeroNegroTest.java
@@ -16,6 +16,7 @@ public class AgujeroNegroTest extends TestCase{
 	private Terreno[][] matrizNivel;
 	private Personaje[] pooglins;
 	private int tamanioMatriz;
+	private int cantidadPooglins;
 	
 	private void llenarMatrizConVacio(){
 		for(int posicionY=1;posicionY<tamanioMatriz-2;posicionY++){
@@ -28,18 +29,21 @@ public class AgujeroNegroTest extends TestCase{
 	public void setUp(){
 		nivel=Nivel.getInstance();
 		tamanioMatriz=20;
-		pooglins=new Pooglin[5];
+		cantidadPooglins=5;
+		pooglins=new Personaje[cantidadPooglins];
 		pooglins[0]=new Pooglin(5,5);
 		pooglins[1]=new Pooglin(7,5);
 		pooglins[2]=new Pooglin(5,6);
 		pooglins[3]=new Pooglin(6,6);
 		pooglins[4]=new Pooglin(7,6);
+		nivel.setCantidadPooglins(cantidadPooglins);
+		nivel.setPooglins(pooglins);
 		matrizNivel=new Terreno[tamanioMatriz][tamanioMatriz];
 		llenarMatrizConVacio();
 		agujeroNegro=new AgujeroNegro(6,5);
 		matrizNivel[6][5]=agujeroNegro;
 		nivel.setMatrizNivel(matrizNivel);
-		nivel.setPooglins(pooglins);
+		
 		
 	}
 	public void testAccionarTerreno(){
