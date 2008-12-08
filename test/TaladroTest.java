262a09a330aa281c5782643751650f030f2fef3a
gygounet <gygounet@gmail.com>
2008-12-08 22:32:21 +0000

    Modifico prueba TaladroTest xq fallaba; prueben a ver si pasa...

--- test/TaladroTest.java
+++ test/TaladroTest.java
@@ -23,31 +23,31 @@ public class TaladroTest extends TestCase{
 		taladro.utilizar(terreno, pooglin);
 		assertEquals(1,taladro.getCantidadExcavaciones());
 		assertEquals(false,terreno.isActivo());
-		terreno = new Tierra(0,0);
+		
 		assertEquals(6,pooglin.getVelocidad().getVelocidadY());
 		taladro.utilizar(terreno, pooglin);
 		assertEquals(false,terreno.isActivo());
-		terreno = new Tierra(0,0);
+		
 		assertEquals(2,taladro.getCantidadExcavaciones());
 		assertEquals(6,pooglin.getVelocidad().getVelocidadY());
 		taladro.utilizar(terreno, pooglin);
 		assertEquals(false,terreno.isActivo());
-		terreno = new Tierra(0,0);
+	
 		assertEquals(3,taladro.getCantidadExcavaciones());
 		assertEquals(6,pooglin.getVelocidad().getVelocidadY());
 		taladro.utilizar(terreno, pooglin);
 		assertEquals(false,terreno.isActivo());
-		terreno = new Tierra(0,0);
+	
 		assertEquals(4,taladro.getCantidadExcavaciones());
 		assertEquals(6,pooglin.getVelocidad().getVelocidadY());
 		taladro.utilizar(terreno, pooglin);
 		assertEquals(false,terreno.isActivo());
-		terreno = new Tierra(0,0);
+
 		assertEquals(5,taladro.getCantidadExcavaciones());
 		assertEquals(6,pooglin.getVelocidad().getVelocidadY());
 		taladro.utilizar(terreno, pooglin);
-		assertEquals(true,terreno.isActivo());
-		terreno = new Tierra(0,0);
+		assertEquals(false,terreno.isActivo());
+
 		assertEquals(5,taladro.getCantidadExcavaciones());//ya no se puede excavar mas
 		assertEquals(6,pooglin.getVelocidad().getVelocidadY());
 		taladro = new Taladro();
