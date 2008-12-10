849d8c80a2bb63957bf67f7008508593bd3abd97
martineq <martineq@gmail.com>
2008-12-10 23:44:55 +0000

    Agregados los 10 Niveles del juego. Modificado el método de "creación" de escenarios.-

--- PruebasDelJuego/pruebaMenu/MouseParaMenu.java
+++ PruebasDelJuego/pruebaMenu/MouseParaMenu.java
@@ -21,37 +21,44 @@ public class MouseParaMenu extends MouseAdapter {
 		public void mouseClicked(MouseEvent arg0) {
 		CargarNivel ejecutarNivel = new CargarNivel();
 		JButton nombre = (JButton)arg0.getSource();
-		String ubicacionNivelXml = "Nivel0.xml";
+		String ubicacionNivelXml = "niveles\\Nivel0.xml";
 		
 		if(nombre.getText()== "Jugar") 	{
-			System.out.println("Falta Cargar el Nivel: "+ ((VentanaInicio)ventana).getNumeroDeNivel());
-			System.out.println("implementar logica en Mouse para menu");			
-			
 			switch (((VentanaInicio)ventana).getNumeroDeNivel()){
+			case 0:
+				ubicacionNivelXml = "niveles\\Nivel0.xml";
+				break;
 			case 1:
-				ubicacionNivelXml = "Nivel1.xml";
+				ubicacionNivelXml = "niveles\\Nivel1.xml";
 				break;
 			case 2:
-				ubicacionNivelXml = "Nivel2.xml";
+				ubicacionNivelXml = "niveles\\Nivel2.xml";
 				break;
 			case 3:
-				ubicacionNivelXml = "Nivel3.xml";
+				ubicacionNivelXml = "niveles\\Nivel3.xml";
 				break;
 			case 4:
-				ubicacionNivelXml = "Nivel4.xml";
+				ubicacionNivelXml = "niveles\\Nivel4.xml";
 				break;
 			case 5:
-				ubicacionNivelXml = "Nivel5.xml";
+				ubicacionNivelXml = "niveles\\Nivel5.xml";
+				break;
+			case 6:
+				ubicacionNivelXml = "niveles\\Nivel6.xml";
+				break;
+			case 7:
+				ubicacionNivelXml = "niveles\\Nivel7.xml";
+				break;
+			case 8:
+				ubicacionNivelXml = "niveles\\Nivel8.xml";
+				break;
+			case 9:
+				ubicacionNivelXml = "niveles\\Nivel9.xml";
 				break;
 			}
-			
-			
 			ejecutarNivel.setNombreDelXML(ubicacionNivelXml);
 			TiempoEnSegundos.getInstance().comenzarTiempo();
 			ejecutarNivel.start();
-		
-		
-		
 		}
 		
 		if(nombre.getText()== "Cargar Nivel") 	{
@@ -60,7 +67,7 @@ public class MouseParaMenu extends MouseAdapter {
 		
 		if(nombre.getText()== "Salir") 	{
 			System.out.println("Guardar antes de salir");
-			if(Nivel.getInstance().isCargado()) Nivel.getInstance().guardarXML("Guardado.xml");
+			if(Nivel.getInstance().isCargado()) Nivel.getInstance().guardarXML("niveles\\Guardado.xml");
 			ventana.dispose();
 			System.exit(0);
 		}
@@ -72,7 +79,6 @@ public class MouseParaMenu extends MouseAdapter {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		CargarNivel ejecutarNivel = new CargarNivel();
-		
 		int result= fileChooser.showOpenDialog(ventana);
 		if (result== JFileChooser.CANCEL_OPTION) return;
 		File name= fileChooser.getSelectedFile();
@@ -82,10 +88,9 @@ public class MouseParaMenu extends MouseAdapter {
 				TiempoEnSegundos.getInstance().comenzarTiempo();
 				ejecutarNivel.start();
 				}
-			}
-			else {
-				JOptionPane.showMessageDialog(null," No existe "," Error ",JOptionPane.ERROR_MESSAGE);
-			}
+		}else{
+			JOptionPane.showMessageDialog(null," No existe "," Error ",JOptionPane.ERROR_MESSAGE);
+			 }
 		}
 	}
 
